11e1dc2ddaa8f9fab6cd79c7135a21f631535e1f
wuko233 <wuko233@gmail.com>
2025-02-16 13:53:03 +0800

    test ignore

--- config.h
+++ config.h
@@ -2,9 +2,9 @@
 #include <Arduino.h>
 
 // WiFi Configs
-#define ssid_Router ""
-#define password_Router ""
+#define ssid_Router "1"
+#define password_Router "1"
 
 //AI Reply Configs
-#define Api_key "Bearer " // Bearer <token>
-#define Model_name ""
+#define Api_key "Bearer sk-c1ri" // Bearer <token>
+#define Model_name "Pro/deepseek-ai/DeepSeek-R1"
